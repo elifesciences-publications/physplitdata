6d05aa24e10af7b94e6112a708d8461e15459bef
Shahar Frechter <frechter@mrc-lmb.cam.ac.uk>
2015-03-06 14:49:04 +0000

    Add first version of data and document it

---
+++ R/physplitdata.R
@@ -0,0 +1,27 @@
+#' A dump of key cell metadata from the PhySplit Filemaker database
+#' 
+#' This is initally created by DumpDatabase.R
+#' @docType data 
+#' @name PhySplitDB
+NULL
+
+#' All spikes from Shahar's cells
+#' 
+#' This is initally created by CollectSpikes.R. The object is of
+#' class \code{\link[gphys]{spiketimes}}
+#' @docType data 
+#' @name Spikes
+#' @seealso \code{\link{smSpikes}}
+NULL
+
+#' Smoothed PSTH for all spikes from Shahar's cells
+#' 
+#' This is initally created by CollectSpikes.R
+#' 
+#' The psth is calculated for a window 0-3 seconds using a bandwidth of 0.5s and
+#' a time step of 0.05
+#' @docType data
+#' @name smSpikes
+#' @seealso \code{\link{Spikes}}, \code{\link[gphys]{smpsth}},
+#'   \code{\link[STAR]{psth}}
+NULL
