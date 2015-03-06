f6184f90530dd36407f372dfe978df8926318e05
Shahar Frechter <frechter@mrc-lmb.cam.ac.uk>
2015-03-06 15:06:03 +0000

    Add very basic package documentation

--- R/physplitdata.R
+++ R/physplitdata.R
@@ -1,3 +1,13 @@
+#' Packaged and versioned spikes and metadata for Shahar's cells
+#' 
+#' The main objects are \code{\link{Spikes}}, \code{\link{smSpikes}} and \code{\link{PhySplitDB}}
+#' 
+#' This is initally created by make_physplitdata.R
+#' @docType package
+#' @name physplitdata
+NULL
+
+
 #' A dump of key cell metadata from the PhySplit Filemaker database
 #' 
 #' This is initally created by DumpDatabase.R
