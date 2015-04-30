3ab7ffa38445a382366ff9ab9b9558314862a9d7
Shahar Frechter <frechter@mrc-lmb.cam.ac.uk>
2015-04-30 14:08:52 +0100

    auto update from make_physplitdata.R

--- R/physplitdata.R
+++ R/physplitdata.R
@@ -1,7 +1,7 @@
 #' Packaged and versioned spikes and metadata for Shahar's cells
-#' 
+#'
 #' The main objects are \code{\link{Spikes}}, \code{\link{smSpikes}} and \code{\link{PhySplitDB}}
-#' 
+#'
 #' This is initally created by make_physplitdata.R
 #' @docType package
 #' @name physplitdata
@@ -9,32 +9,41 @@ NULL
 
 
 #' A dump of key cell metadata from the PhySplit Filemaker database
-#' 
+#'
 #' This is initally created by DumpDatabase.R
-#' @docType data 
+#' @docType data
 #' @name PhySplitDB
 NULL
 
 #' A dump of passive cell properties
-#' 
+#'
 #' This is initally created by CollectCellProperties
-#' @docType data 
+#' @docType data
 #' @name ravc
 NULL
 
+#' A dump of the final passive cell properties dataframe
+#' This is initally created by CollectCellProperties
+#' @docType data
+#' @name cpdf.summ
+NULL
+
+
+
+
 #' All spikes from Shahar's cells
-#' 
+#'
 #' This is initally created by CollectSpikes.R. The object is of
 #' class \code{\link[gphys]{spiketimes}}
-#' @docType data 
+#' @docType data
 #' @name Spikes
 #' @seealso \code{\link{smSpikes}}
 NULL
 
 #' Smoothed PSTH for all spikes from Shahar's cells
-#' 
+#'
 #' This is initally created by CollectSpikes.R
-#' 
+#'
 #' The psth is calculated for a window 0-3 seconds using a bandwidth of 0.5s and
 #' a time step of 0.05
 #' @docType data
